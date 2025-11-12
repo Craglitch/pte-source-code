045f1fc83f160abeb60681b6d17ce425704968c1
Craglitch <cariuntung92@gmail.com>
2025-11-12 22:46:27 +0800

    ready for vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://your-project.vercel.app', // Change to your repo name
+	output: 'static', // Remove adapter for simplicity
 	integrations: [mdx(), sitemap()],
 });
