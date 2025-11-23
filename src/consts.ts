e9804e4111f1435d21342cc7cf628bc8ef347992
Craglitch <cariuntung92@gmail.com>
2025-11-23 13:17:33 +0800

    Update global meta data

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'PTE SMKP';
+export const SITE_TITLE = 'MPP PTE SMKP';
 export const SITE_DESCRIPTION = 'SELAMAT DATANG KE HALAMAN RASMI PTE PENANTI';
