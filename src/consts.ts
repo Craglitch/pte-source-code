e87063367e451646cc867b0d0b0e5205db026d9a
Craglitch <cariuntung92@gmail.com>
2025-12-19 23:41:41 +0800

    Tangguh.exe heheh

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'MPP PTE SMKP';
+export const SITE_TITLE = 'JPTE SMKP';
 export const SITE_DESCRIPTION = 'SELAMAT DATANG KE HALAMAN RASMI PTE PENANTI';
+
+export const SITE_BLOG = "BLOG RASMI JPTE PENANTI";
