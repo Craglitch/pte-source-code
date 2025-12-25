dea2f92b253734009657da0984a9d8eb4154634c
Craglitch <cariuntung92@gmail.com>
2025-12-25 15:05:20 +0800

    SEO search engine deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,23 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import robotsTxt from 'astro-robots-txt';
 
 export default defineConfig({
-	site: 'https://your-project.vercel.app', // Change to your repo name
+	site: 'https://penanti-pte.vercel.app', // Change to your repo name
 	output: 'static', // Remove adapter for simplicity
-	integrations: [mdx(), sitemap()],
+	integrations: [
+    mdx(), 
+    sitemap(),
+    robotsTxt({
+      sitemap: true, // Auto-link to sitemap
+      policy: [
+        {
+          userAgent: '*',
+          allow: '/',
+          disallow: ['/admin/', '/dashboard/', '/api/'],
+        }
+      ]
+    })
+  ],
 });
