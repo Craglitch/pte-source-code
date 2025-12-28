71854999a4ece0519d576e7f63287e6951027d94
Craglitch <cariuntung92@gmail.com>
2025-12-29 00:10:44 +0800

    const update

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'JPTE SMKP';
-export const SITE_DESCRIPTION = 'SELAMAT DATANG KE HALAMAN RASMI PTE PENANTI';
+export const SITE_DESCRIPTION = 'SELAMAT DATANG KE HALAMAN RASMI JPTE PENANTI. Laman ini berfungsi sebagai platform utama penyampaian maklumat, hebahan aktiviti, pengumuman rasmi serta perkongsian pencapaian JPTE Penanti. Ia diwujudkan bagi memastikan komunikasi yang lebih berkesan antara pihak pengurusan, ahli jawatankuasa, pelajar, dan komuniti setempat. Melalui halaman ini, segala perkembangan semasa, program yang dianjurkan, serta inisiatif berkaitan pendidikan, teknologi dan pembangunan sahsiah akan dikemaskini secara berkala sebagai rujukan dan manfaat bersama.';
 
 export const SITE_BLOG = "BLOG RASMI JPTE PENANTI";
